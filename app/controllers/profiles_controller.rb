422283ce1c56b6c58aa37b7a1755b87f4da1581a
tomBeach <tbeach2k@gmail.com>
2015-08-14 11:03:22 -0400

    fixed profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,9 +8,21 @@ class ProfilesController < ApplicationController
 
   def new
     @profile = Profile.new
+    @user = current_user
   end
 
   def create
+    @profile = Profile.new(profile_params)
+
+    respond_to do |format|
+      if @profile.save
+        format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
+        format.json { render :show, status: :created, location: @profile }
+      else
+        format.html { render :new }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -21,5 +33,11 @@ class ProfilesController < ApplicationController
 
   def delete
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def profile_params
+      params.require(:profile).permit(:username, :firstname, :lastname, :address, :phone, :race, :ethnicity, :sex, :DOB)
+    end
 
 end
